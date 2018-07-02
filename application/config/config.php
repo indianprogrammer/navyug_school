cfd866c66f80b3c02dd22b6725517bd6d3a2912f
root <root@localhost.localdomain>
2018-07-02 23:44:51 +0530

    base url changed

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/hmvc_institute/';
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/project/';
+
+$config['modules_locations'] = array(APPPATH.'modules/' => '../modules/');
 
 /*
 |--------------------------------------------------------------------------
