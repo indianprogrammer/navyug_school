6fbdf16c40802633498217077479dba52470a62d
creatorvivek <vivek.et19932gmail.com>
2018-07-03 15:37:04 +0530

    add admin panel

--- application/modules/institute/controllers/Welcome.php
+++ application/modules/institute/controllers/Welcome.php
@@ -21,6 +21,6 @@ class Welcome extends MX_Controller {
 	public function index()
 	{
 		// $this->load->view('welcome_message');
-		$this->load->view('index');
+		$this->load->view('admin/index2');
 	}
 }
---
+++ application/modules/institute/views/admin/index3.php
@@ -0,0 +1,13 @@
+<?php include_once dirname(__FILE__) .'/header.php'; ?>
+<?php include_once dirname(__FILE__) .'/sidenav.php'; ?>
+
+	<div class="row">
+		<div class="col-md-6 ">
+			<div class="col-md-2"><div class="box" style="height: 100px;width: 80px;background-color: red">Student</div></div>
+			<div class="box" style="height: 100px;width: 100px;background-color: red">Student</div>
+			<div class="col-md-2"><div class="box" style="height: 100px;width: 100px;background-color: red">Student</div></div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
