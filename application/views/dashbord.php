325a149c9f8bbef1d8331d942af7850bf3ca3b0e
vivekcreator <vivek.et1993@gmail.com>
2018-08-04 08:50:23 +0530

    tooltip on dasboard

--- application/views/dashbord.php
+++ application/views/dashbord.php
@@ -23,7 +23,7 @@
             <!-- Info boxes -->
             <div class="row">
                 <div class="col-12 col-sm-6 col-md-3">
-                 <a href="<?= base_url() ?>subject/subject_list">  
+                 <a href="<?= base_url() ?>subject/subject_list" data-toggle="tooltip" data-placement="top" title="click here for more information">  
                     <div class="info-box">
                         <span class="info-box-icon bg-info elevation-1"><i class="fa fa-book"></i></span>
 
@@ -52,7 +52,7 @@
         </div>
         <!-- /.col -->
         <div class="col-12 col-sm-6 col-md-3">
-          <a href="<?= base_url() ?>student/student_list">  
+          <a href="<?= base_url() ?>student/student_list" data-toggle="tooltip" data-placement="top" title="click here for more information">  
             <div class="info-box mb-3">
                 <span class="info-box-icon bg-danger elevation-1"><i class="fa fa-child"></i></span>
 
@@ -83,7 +83,7 @@
     <div class="clearfix hidden-md-up"></div>
 
     <div class="col-12 col-sm-6 col-md-3">
-     <a href="<?= base_url() ?>classes/class_list">  
+     <a href="<?= base_url() ?>classes/class_list" data-toggle="tooltip" data-placement="top" title="click here for more information">  
         <div class="info-box mb-3">
             <span class="info-box-icon bg-success elevation-1"><i class="fa fa-shopping-cart"></i></span>
 
@@ -111,7 +111,7 @@
 </div>
 <!-- /.col -->
 <div class="col-12 col-sm-6 col-md-3">
- <a href="<?= base_url() ?>employee/employee_list">  
+ <a href="<?= base_url() ?>employee/employee_list" data-toggle="tooltip" data-placement="top" title="click here for more information">  
     <div class="info-box mb-3">
         <span class="info-box-icon bg-warning elevation-1"><i class="fa fa-users"></i></span>
 
