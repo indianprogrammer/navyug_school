b1a5eba990cd76df2e4d4d0a835907b3c1ce47fd
vivekcreator <vivek.et1993@gmail.com>
2018-08-13 22:10:31 +0530

    commit

--- application/config/config.php
+++ application/config/config.php
@@ -25,8 +25,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 
-  // $config['base_url'] = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
-$config['base_url'] = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST']."/navyug_school/";
+  $config['base_url'] = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
+// $config['base_url'] = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST']."/navyug_school/";
 
 
 $config['modules_locations'] = array(APPPATH.'modules/' => '../modules/');
